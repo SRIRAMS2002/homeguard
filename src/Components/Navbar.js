33fe060ccb7ee3917cd5450994c587943c4f7be1
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2023-10-04 15:29:15 +0530

    home-3

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,164 +1,156 @@
-import React from 'react'
-import { Fragment, useState } from 'react'
-import { Dialog,Menu, Transition } from '@headlessui/react'
-import {
-  Bars3Icon,
-  XMarkIcon,
-  BellIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid';
-import {GiStaticGuard} from "react-icons/gi";
-import ProPic from '../../public/Sriram.jpg';
-import Image from 'next/image';
-import Logo from '../../public/logo.png';
+import React from "react";
+import { Fragment, useState } from "react";
+import { Dialog, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon, BellIcon } from "@heroicons/react/24/outline";
 
-
+import Image from "next/image";
+import Logo from "../../public/logo.png";
+import Profile from '../../public/cool-profile-pictures-aperture.webp';
 const Navbar = () => {
-    
-  const user = {
-    name: 'Tom Cook',
-    email: 'tom@example.com',
-    
-  }
-  const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-    { name: 'Reports', href: '#', current: false },
-  ]
   const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
-  ] 
-      
-      function classNames(...classes) {
-        return classes.filter(Boolean).join(' ')
-      }
-      const [mobileMenuOpen, setMobileMenuOpen] = useState(false)      
-  return (
-    <div className=''>
-        <header className=" relative">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5 flex items-center">
-            <span className="sr-only">Your Company</span>
-            <Image src={Logo} className='h-8 w-8' />
-           <h1  className="text-3xl flex space-x-5 font-Outfit  leading-6 text-gray-900 font-extrabold"> <span className='text-black'> </span>HomeGuard</h1>
-          </a>
-        </div>
-       
-        <div className='hidden lg:flex space-x-10 text-lg items-center'>
-        <a href="/Request" className="text-lg font-semibold leading-6  text-white border-2 border-gray-900 bg-gray-800 rounded-xl p-2 ">
-            Request Demo
-          </a>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div className="hidden lg:block">
-                    <div className="ml-4 flex items-center md:ml-6">
-                      <button
-                        type="button"
-                        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
+    { name: "Your Profile", href: "#" },
+    { name: "Settings", href: "#" },
+    { name: "Sign out", href: "#" },
+  ];
 
-                      {/* Profile dropdown */}
-                      <Menu as="div" className="relative ml-3">
-                        <div>
-                          <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span className="absolute -inset-1.5" />
-                            <span className="sr-only">Open user menu</span>
-                             <Image className="h-8 w-8 rounded-full" src={ProPic} alt="" /> 
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
-                        >
-                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
-                    </div>
-                  </div>
-        
-      </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="mx-auto fixed right-5 top-5">
-            
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <div className="">
+      <header className=" relative">
+        <nav
+          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5 flex items-center">
+              <span className="sr-only">Your Company</span>
+              <Image src={Logo} className="h-8 w-8" />
+              <h1 className="text-3xl flex space-x-5 font-Outfit  leading-6 text-gray-900 font-extrabold">
+                {" "}
+                <span className="text-black"> </span>HomeGuard
+              </h1>
+            </a>
+          </div>
+
+          <div className="hidden lg:flex space-x-10 text-lg items-center">
+            <a
+              href="/Request"
+              className="bg-green-600 text-white text-normal md:text-xl font-semibold p-2 rounded-md  hover:bg-green-700"
+            >
+              Request Demo
+            </a>
+          </div>
+          <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
             >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+          <div className="hidden lg:block">
+            <div className="ml-4 flex items-center md:ml-6">
+              <button
+                type="button"
+                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              >
+                <span className="absolute -inset-1.5" />
+                <span className="sr-only">View notifications</span>
+                <BellIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+
+              {/* Profile dropdown */}
+              <Menu as="div" className="relative ml-3">
+                <div>
+                  <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">Open user menu</span>
+                    <Image
+                      className="h-10 w-9 rounded-full"
+                     src={Profile}
+                      alt="jij"
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
                 >
-                  Request Demo
-                </a>
-               
-                
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log In / Sign Up
-                </a>
-              </div>
+                  <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    {userNavigation.map((item) => (
+                      <Menu.Item key={item.name}>
+                        {({ active }) => (
+                          <a
+                            href={item.href}
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            {item.name}
+                          </a>
+                        )}
+                      </Menu.Item>
+                    ))}
+                  </Menu.Items>
+                </Transition>
+              </Menu>
             </div>
           </div>
-        </Dialog.Panel>
-      </Dialog>
-    </header>
-      
+        </nav>
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
+          <div className="fixed inset-0 z-10" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="mx-auto fixed right-5 top-5">
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Request Demo
+                  </a>
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Log In / Sign Up
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -283,50 +283,88 @@ const index = () => {
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
        
 
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-         
-            <div className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <Image
-                  src={Demo1}
-                 alt="Tools"
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              
+      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        
+           
+
+        <div className="group relative">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <Image
+              src={Demo1}
+              alt="jhi"
+              className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+          </div>
+          <div className="justify-center">
+            <div>
+              <h3 className="text-lg font-medium mx-auto text-center text-gray-700">
+               Tools and Technologies
+              </h3>
+            
             </div>
-            <div className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <Image
-                  src={Demo2}
-                 alt='Appliances'
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              
-            </div>
-            <div className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <Image
-                  src={Demo3}
-                 alt='Customer Care'
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              
-            </div>
-            <div className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <Image
-                  src={Demo4 }
-                 alt='Message'
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              
-            </div>
-         
+           
+          </div>
         </div>
+
+        <div className="group relative">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <Image
+              src={Demo2}
+              alt="jhi"
+              className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+          </div>
+          <div className="  justify-center">
+            <div>
+              <h3 className="text-lg font-medium mx-auto text-center text-gray-700">
+               Appliances
+              </h3>
+            
+            </div>
+           
+          </div>
+        </div>
+
+        <div className="group relative">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <Image
+              src={Demo3}
+              alt="jhi"
+              className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+          </div>
+          <div className="  justify-center">
+            <div>
+              <h3 className="text-lg font-medium mx-auto text-center text-gray-700">
+               Customer Service
+              </h3>
+            
+            </div>
+           
+          </div>
+        </div>
+
+        <div className="group relative">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <Image
+              src={Demo4}
+              alt="jhi"
+              className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+          </div>
+          <div className="  justify-center">
+            <div>
+              <h3 className="text-lg font-medium mx-auto text-center text-gray-700">
+               24/7 Message Query
+              </h3>
+            
+            </div>
+           
+          </div>
+        </div>
+
+      
+    </div>
       </div>
     </div>                
 
@@ -348,13 +386,13 @@ const index = () => {
 
           {/* POSTAL CODE */}
 
-          <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
+          <div className="relative isolate overflow-hidden bg-gradient-to-b from-white via-green-200 to-white py-16 sm:py-24 lg:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
           <div className="max-w-xl lg:max-w-lg">
-            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Checkout here <span className='text-md'>&#128071;</span> </h2>
-            <p className="mt-4 text-lg leading-8 text-gray-200">
-            Buy Today, Ships Next Business day with <span className='font-bold text-slate-400'>Free Delivery</span>  included
+            <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">Checkout here <span className='text-md'>&#128071;</span> </h2>
+            <p className="mt-4 text-lg leading-8 text-gray-800">
+            Buy Today, Ships Next Business day with <span className='font-bold text-slate-800'>Free Delivery</span>  included
             </p>
             <div className="mt-6 flex max-w-md gap-x-4">
               <label htmlFor="email-address" className="sr-only">
@@ -365,13 +403,13 @@ const index = () => {
                 type="text"
                
                
-                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                className="min-w-0 flex-auto rounded-md border-2 bg-white/5 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 placeholder="Enter your Postcode"
               />
               <button
                 type="submit"
                 onClick={() => setOpen(true)}
-                className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Get a Quote
               </button>
@@ -413,7 +451,7 @@ const index = () => {
                         Delivery status
                       </Dialog.Title>
                       <div className="mt-2">
-                        <p className="text-sm text-gray-500">
+                        <p className="text-sm text-gray-800">
                         Thank you for your Interest. Product will be shift between 5 - 6 Business days
                         </p>
                       </div>
@@ -423,7 +461,7 @@ const index = () => {
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
+                    className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
                     onClick={() => setOpen(false)}
                   >
                     Submit
@@ -441,19 +479,19 @@ const index = () => {
           <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
             <div className="flex flex-col items-start">
               <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                <CalendarDaysIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                <CalendarDaysIcon className="h-6 w-6 text-black" aria-hidden="true" />
               </div>
-              <dt className="mt-4 font-semibold text-white">Our Vision</dt>
-              <dd className="mt-2 leading-7 text-gray-400">
+              <dt className="mt-4 font-semibold text-black">Our Vision</dt>
+              <dd className="mt-2 leading-7 text-gray-800">
               Enhancing individuals' everyday experiences by empowering all to make informed energy decisions. Taking care of our planet commences with your actions.
               </dd>
             </div>
             <div className="flex flex-col items-start">
               <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                <HandRaisedIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                <HandRaisedIcon className="h-6 w-6 text-black" aria-hidden="true" />
               </div>
-              <dt className="mt-4 font-semibold text-white">Our Mission</dt>
-              <dd className="mt-2 leading-7 text-gray-400">
+              <dt className="mt-4 font-semibold text-black">Our Mission</dt>
+              <dd className="mt-2 leading-7 text-gray-800">
               We offer an integrated solution ecosystem that is effortlessly embraced by individuals across the board.
               </dd>
             </div>
@@ -462,7 +500,7 @@ const index = () => {
       </div>
       <div className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
         <div
-          className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+          className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#090814] to-[#ebdcfd] opacity-30"
           style={{
             clipPath:
               'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
@@ -472,7 +510,12 @@ const index = () => {
     </div> 
 
 
-      
+            {/* Demo */}
+
+         
+
+
+
     </div>
   )
 }
